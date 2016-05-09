9efd594fb6f524167d92104d7fbbcb1bbd230cf0
José Pedro <1jpsneto@gmail.com>
2016-05-09 11:02:46 -0300

    Consertando os graficos de Acelerancia

--- codigos/codigo_trabalho_vibracao.m
+++ codigos/codigo_trabalho_vibracao.m
@@ -55,7 +55,7 @@ frequencias_naturais = diag(omega_n)/(2*pi);
 fatores_amortecimento_modais = (alpha_proporcional + omega_n_quadrado*beta_proporcional)/(2*omega_n);
 fatores_amortecimento_modais = diag(fatores_amortecimento_modais);
 
-% Construindo a forca de excitacao
+% Construindo resposta em frequencia (H)
 delta_frequencia = 0.1;
 tempo_total = 1/delta_frequencia; % unidade em segundos
 frequencia_amostragem = 44100/44; % 44100 amostras por segundo
@@ -63,8 +63,6 @@ frequencias_excitacao = [10:delta_frequencia:250]; % Hz
 delta_tempo = 1/frequencia_amostragem;
 tempos = [0:delta_tempo:tempo_total];
 frequencias_excitacao = (0:length(tempos)-1)*frequencia_amostragem/length(tempos); 
-
-% Construindo resposta em frequencia (H)
 omegas = 2*pi*frequencias_excitacao;
 omegas_quadrado = omegas.^2;
 H(numero_graus_liberdade, numero_graus_liberdade, length(omegas)) = 0;
@@ -80,38 +78,58 @@ for omega = 1:length(omegas)
 	(1./(omega_n_quadrado - omegas(omega).^2))*modos_vibracao';
 end
 
+%--
 % Extraindo a resposta em 2 e excitação em 2
-local_aplicacao_forca = 2;
-local_captacao_resposta = 2;
+local_resposta = 2;
+local_excitacao = 2;
 H_22(1:length(omegas)) = 0;
 H_22_sem_amortecimento(1:length(omegas)) = 0; 
 for n = 1:length(omegas)
-	H_22(n) = abs(H(local_captacao_resposta,local_aplicacao_forca,n));
+	H_22(n) = abs(H(local_resposta, local_excitacao, n));
 	H_22_sem_amortecimento(n) = abs(H_sem_amortecimento(2,2,n)); 
 end
+% Calculando a acelerancia para resposta em 2 e excitação em 2
+A_22 = omegas_quadrado.*H_22;
+A_22_sem_amortecimento = (omegas.^2).*H_22_sem_amortecimento;
 
-
-
-figure;
-semilogy(frequencias_excitacao, H_22, 'black');
+% Plotando Acelerancia numa Excitacao em 2 e Resposta Captada em 2
+figure(1);
+semilogy(frequencias_excitacao, A_22, 'black');
 set(findobj(gca,'type','line'), 'LineWidth', 3);
 hold on;
-semilogy(frequencias_excitacao, H_22_sem_amortecimento, 'blue');
-axis([0 250 0 0.0001])
+loglog(frequencias_excitacao, A_22_sem_amortecimento, 'blue');
+axis([10 250 0 max(A_22_sem_amortecimento)]);
+title( ... 
+'Magnitude do Espectro de Frequencias da Acelerancia numa Excitacao em 2 e Resposta em 2', ... 
+'Interpreter','latex','FontSize',16);
+xlabel('Frquencias [Hz]','Interpreter','latex','FontSize',16); 
+ylabel('Amplitude','Interpreter','latex','FontSize',16);
+legend('Acelerancia com Amortecimento','Acelerancia sem Amortecimento');
 
-
+%--
 % Extraindo a resposta em 4 e excitação em 2
+local_resposta = 4;
+local_excitacao = 2;
 H_42(1:length(omegas)) = 0;
 H_42_sem_amortecimento(1:length(omegas)) = 0;
 for n = 1:length(omegas)
-	H_42(n) = abs(H(4,local_aplicacao_forca,n));
+	H_42(n) = abs(H(local_resposta, local_excitacao, n));
 	H_42_sem_amortecimento(n) = abs(H_sem_amortecimento(4,2,n));
 end
+% Calculando a acelerancia para resposta em 4 e excitação em 2
+A_42 = omegas_quadrado.*H_42;
+A_42_sem_amortecimento = (omegas.^2).*H_42_sem_amortecimento;
 
-figure;
-semilogy(frequencias_excitacao, H_42, 'black');
+% Plotando Acelerancia numa Excitacao em 4 e Resposta Captada em 2
+figure(2);
+semilogy(frequencias_excitacao, A_42, 'black');
 set(findobj(gca,'type','line'), 'LineWidth', 3);
 hold on;
-semilogy(frequencias_excitacao, H_42_sem_amortecimento, 'blue');
-axis([0 250 0 0.0001])
-
+loglog(frequencias_excitacao, A_42_sem_amortecimento, 'blue');
+axis([10 250 0 max(A_42_sem_amortecimento)]);
+title( ... 
+'Magnitude do Espectro de Frequencias da Acelerancia numa Excitacao em 4 e Resposta em 2', ... 
+'Interpreter','latex','FontSize',16);
+xlabel('Frquencias [Hz]','Interpreter','latex','FontSize',16); 
+ylabel('Amplitude','Interpreter','latex','FontSize',16);
+legend('Acelerancia com Amortecimento','Acelerancia sem Amortecimento');
